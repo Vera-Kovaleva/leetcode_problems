33a9667be31478f8f9c033d32aba1648da8c97e7
Vera Kovaleva <vekovaleva@edu.hse.ru>
2025-07-09 15:45:16 +0300

    new problem solved;)

---
+++ 345_reverse_vowels_of_a_string/go.mod
@@ -0,0 +1,3 @@
+module 345_reverse_vowels_of_a_string
+
+go 1.24.4
---
+++ 345_reverse_vowels_of_a_string/solution.go
@@ -0,0 +1,25 @@
+package main
+
+func isVowel(c rune) bool {
+	return c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' || c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+}
+
+func reverseVowels(s string) string {
+	result := []rune(s)
+
+	iStart := 0
+	iEnd := len(s) - 1
+	for iStart < iEnd {
+		for !isVowel(result[iStart]) && iStart < iEnd {
+			iStart++
+		}
+		for !isVowel(result[iEnd]) && iStart < iEnd {
+			iEnd--
+		}
+		result[iEnd], result[iStart] = result[iStart], result[iEnd]
+		iStart++
+		iEnd--
+	}
+
+	return string(result)
+}
